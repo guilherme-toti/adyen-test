f9af41b4e94a48b14a36933e58b32ec31a80c6f3
Guilherme Toti <guilhermetoti@gmail.com>
2020-03-26 08:22:34 -0300

    Updates

---
+++ precache-manifest.a7a25254fc04a7c360bb71331d4565e2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829c0872e736986f9835426a82cd8102",
+    "url": "/adyen-test/index.html"
+  },
+  {
+    "revision": "df6466beac873614d403",
+    "url": "/adyen-test/static/css/main.42f43015.chunk.css"
+  },
+  {
+    "revision": "be301a59bdb020671c1e",
+    "url": "/adyen-test/static/js/2.f6fc40a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/adyen-test/static/js/2.f6fc40a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df6466beac873614d403",
+    "url": "/adyen-test/static/js/main.d146afae.chunk.js"
+  },
+  {
+    "revision": "5d944e29e4e02b1baa66",
+    "url": "/adyen-test/static/js/runtime-main.7eaf894c.js"
+  },
+  {
+    "revision": "49819b21c226bdf7015845d4bdf9e273",
+    "url": "/adyen-test/static/media/airplane-unsplash.49819b21.jpg"
+  },
+  {
+    "revision": "91d8ff8ac25f51073a577408ae9cec86",
+    "url": "/adyen-test/static/media/avenir-next-bold.91d8ff8a.otf"
+  },
+  {
+    "revision": "b5ed933057eaf1919dd79d81248cdbce",
+    "url": "/adyen-test/static/media/avenir-next.b5ed9330.otf"
+  },
+  {
+    "revision": "799f5d9d05d340c3ae177864e62ab846",
+    "url": "/adyen-test/static/media/clouds-unsplash.799f5d9d.jpg"
+  },
+  {
+    "revision": "bd6936e4abc7d304ce3f99477d863cfb",
+    "url": "/adyen-test/static/media/home-background.bd6936e4.png"
+  },
+  {
+    "revision": "e39d1c3aa419fa0233e81acd8c721634",
+    "url": "/adyen-test/static/media/tickets-loading.e39d1c3a.svg"
+  },
+  {
+    "revision": "50538102d91329699bac3506d8f2f6cc",
+    "url": "/adyen-test/static/media/weather-loading.50538102.svg"
+  }
+]);
