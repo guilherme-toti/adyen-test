3d2d63abb3eaf8f5bb91caf0cbd97bff5e32665c
Guilherme Toti <guilhermetoti@gmail.com>
2020-03-26 09:41:30 -0300

    Added Postman resources & added FLY_FROM const

--- resources/README.md
+++ resources/README.md
@@ -1,1 +1,4 @@
-This Sketch file was downloaded from [sketchappsources](https://www.sketchappsources.com/free-source/3201-travel-world-landing-page-sketch-freebie-resource.html) and adaptated to this project.
+# Resources
+
+- Sketch file was downloaded from [sketchappsources](https://www.sketchappsources.com/free-source/3201-travel-world-landing-page-sketch-freebie-resource.html) and adaptated to this project.
+- Postman collection and environment
--- src/api/tickets.js
+++ src/api/tickets.js
@@ -2,12 +2,13 @@ import { getCityCode } from '../lib/cities'
 
 import { Tickets } from './models/tickets'
 
+const FLY_FROM = 'AMS'
 const BASE_URL = 'https://api.skypicker.com/'
 
 export const flights = city => {
   const cityCode = getCityCode(city)
 
-  return fetch(`${BASE_URL}flights?flyFrom=AMS&to=${cityCode}&partner=picky`)
+  return fetch(`${BASE_URL}flights?flyFrom=${FLY_FROM}&to=${cityCode}&partner=picky`)
     .then(res => res.json())
     .then(res => Tickets(res))
 }
